96c3a052811e5763b24672f9170fc4609f042cd4
Aliyyan <162626203+Aliyyan007@users.noreply.github.com>
2026-02-17 23:37:12 +0000

    Unwrap shard-modules: move files/folders into repository root

--- shard-modules/enums/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    ...require("./AchievementType"),
-    ...require("./RewardType")
-}
--- shard-modules/schemas/blacklistSchema.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, models } = require('mongoose')
-const blacklistSchema = new Schema({
-	guildID: { type: String },
-	userID: { type: String },
-	full_ban: { type: Boolean }
-})
-const name = "blacklist"
-module.exports = models[name] || model(name, blacklistSchema)
